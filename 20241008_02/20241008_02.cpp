c49fecce8ff701f2402a503792cd7f05201d8326
Saki Yamada <sichimi_0331-school@yahoo.co.jp>
2024-10-09 20:57:21 +0900

    プロジェクト ファイルを追加します。

---
+++ 20241008_02/20241008_02.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int Sum(int x, int y);
+
+int main()
+{
+	int x = 3, y = 5;
+
+	int ans = Sum(x, y);
+
+	cout << x << " + " << y << " = " << ans << endl;
+}
---
+++ 20241008_02/sum.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int Sum(int x, int y)
+{
+	return x + y;
+}
